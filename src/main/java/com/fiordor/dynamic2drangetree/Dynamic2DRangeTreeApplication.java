17cf75d6d6f35ec59eaa9ff218e72d2fe94a8c9f
fiordor <1fiordor@gmail.com>
2022-10-14 19:33:39 +0200

    genera imagenes

--- src/main/java/com/fiordor/dynamic2drangetree/Dynamic2DRangeTreeApplication.java
+++ src/main/java/com/fiordor/dynamic2drangetree/Dynamic2DRangeTreeApplication.java
@@ -8,10 +8,17 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Base64;
 import java.util.Map;
 import java.util.Scanner;
+
+import javax.imageio.ImageIO;
 
 @SpringBootApplication
 @RestController
@@ -23,17 +30,47 @@ public class Dynamic2DRangeTreeApplication {
 
 	@GetMapping("/hello")
 	public String sayHello() {
-		String html = readFile();
+		String html = readResourceFile();
 		//html = String.format(html, "c");
 		return html;
 	}
 
 	@PostMapping("/test")
 	public String test (@RequestParam Map<String, String> params) {
-		return params.toString();
+		
+		return createImage(params.toString(), true);
+
 	}
 
-	private String readFile() {
+	/**
+	 * Create a image using text
+	 * 
+	 * @param text Text to draw
+	 * @param html Specify if the base64 string is for HTML use
+	 * @return image on base64 String
+	 */
+	private String createImage(String text, boolean html) {
+
+		final String FORMAT = "png";
+
+		BufferedImage bufferedImage = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
+		Graphics2D g2d = bufferedImage.createGraphics();
+		g2d.setColor(Color.WHITE);
+		g2d.drawString(text, 10, 10);
+
+		String imgBase64 = "";
+		try {
+			ByteArrayOutputStream byteArray = new ByteArrayOutputStream();
+			ImageIO.write(bufferedImage, FORMAT, byteArray);
+			imgBase64 = Base64.getEncoder().encodeToString(byteArray.toByteArray());
+		} catch (IOException e) {
+			imgBase64 = e.getMessage();
+		}
+
+		return html ? "data:image/" + FORMAT + ";base64," + imgBase64 : imgBase64;
+	}
+
+	private String readResourceFile() {
 		try {
 			InputStream fileObject = new ClassPathResource("templates/index.html").getInputStream();
 			Scanner fileReader = new Scanner(fileObject);
